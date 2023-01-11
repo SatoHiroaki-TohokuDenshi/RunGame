958795ac7622eba7af608c9149824f46f6f00427
央旺 佐藤 <220511@jc-21.jp>
2023-01-11 12:32:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.3
+
+void Main()
+{
+	// 背景の色を設定 | Set background color
+	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	// 通常のフォントを作成 | Create a new font
+	const Font font{ 60 };
+
+
+	while (System::Update())
+	{
+		
+	}
+}
