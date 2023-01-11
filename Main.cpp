91f2ccee4ba4eb9c2a30abf06bd100c28ace74d7
央旺 佐藤 <220511@jc-21.jp>
2023-01-11 14:27:07 +0900

    mainの更新

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,20 @@
 ﻿# include "SceneBase.h"
+# include "TitleScene.h"
+# include "PlayScene.h"
+# include "ScoreScene.h"
 
 void Main()
 {
-	// 背景の色を設定 | Set background color
-	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
-	// 通常のフォントを作成 | Create a new font
-	const Font font{ 60 };
-
+	App manager;
+	manager.add<Title>(State::Title);
+	manager.add<Play>(State::Play);
+	manager.add<Score>(State::Score);
 
 	while (System::Update())
 	{
-		
+		if (not manager.update())
+		{
+			break;
+		}
 	}
 }
