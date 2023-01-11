6a9a03133da1e4d42db38e901b92dc1dd650ef41
央旺 佐藤 <220511@jc-21.jp>
2023-01-11 16:33:09 +0900

    ジャンプ処理の追加

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,13 +1,37 @@
 ﻿# include "PlayScene.h"
 
 Play::Play(const InitData& init)
-	: IScene{ init }, PlayerChar_{ U"Images/Man_Run1.png", TextureDesc::Mipped } {}
+	: IScene{ init }, PlayerChar_{ U"Images/Man_Run1.png", TextureDesc::Mipped }, GA_(1.0),
+	PlayerCharCoordinate_(40, 450), camera_(0, 0), move_(0, 0)
+{
+}
 
 void Play::update()
 {
+	if (PlayerCharCoordinate_.y != 450)
+	{
+		PlayerCharCoordinate_ += move_;
+		move_.y += GA_;
+	}
+
+	if (PlayerCharCoordinate_.y == 450)
+	{
+		if (KeySpace.down())
+		{
+			move_.y = -21.0;
+			PlayerCharCoordinate_ += move_;
+		}
+	}
+
+	if (PlayerCharCoordinate_.y >= 450)
+	{
+		move_.y = 0;
+		PlayerCharCoordinate_.y = 450;
+	}
 }
 
 void Play::draw() const
 {
-	PlayerChar_.scaled(0.3).draw(40, 450);
+	PlayerChar_.scaled(0.3).draw(PlayerCharCoordinate_);
+
 }
--- PlayScene.h
+++ PlayScene.h
@@ -13,6 +13,9 @@ public:
 	void draw() const override;
 
 private:
-	const Texture PlayerChar_;
-	
+	const Texture PlayerChar_;		//プレイヤーキャラ
+	const double GA_;				//重力加速度
+	Vec2 PlayerCharCoordinate_;		//プレイヤーキャラの座標
+	Vec2 camera_;					//表示位置
+	Vec2 move_;
 };
