b974c7733196c367a98906e6f62daca8b02ec102
央旺 佐藤 <220511@jc-21.jp>
2023-01-18 13:20:21 +0900

    時間の処理、スクロールの処理の実装

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,53 +1,80 @@
 ﻿# include "PlayScene.h"
 
 Play::Play(const InitData& init)
-	: IScene{ init }, GA_(1.0), camera_(0, 0), move_(0, 0),
-	 PlayerPos_(40, 450), MoneyPos_(300, 450),sec_(30),
+	: IScene{ init }, GA_(0.5), Camera_(0, 0), Move_(0), Velocity_(3), MoveDist_(0),
+	 PlayerPos_(40, 350), MoneyPos_(300, 450),Sec_(30),
 	 LifePos_(200, 200),
+	 BackGround_{ U"Images/Field.png", TextureDesc::Mipped },
 	 PlayerChar_{ U"Images/Man_Run1.png", TextureDesc::Mipped },
+	 Tree_{ U"Images/tree.png", TextureDesc::Mipped },
 	 ItemMoney_{ U"Images/money_item.png", TextureDesc::Mipped },
 	 Life_{ U"Images/Life.png", TextureDesc::Mipped }
 {
-	//Stopwatch stopwatch_{ StartImmediately::Yes };
+	TreePos_[0] = Vec2(100, 300);
+	TreePos_[1] = Vec2(400, 300);
+	TreePos_[2] = Vec2(700, 300);
 }
 
 void Play::update()
 {
 	//ジャンプの処理
-	if (PlayerPos_.y != 450)
+	if (PlayerPos_.y != 350)
 	{
-		PlayerPos_ += move_;
-		move_.y += GA_;
+		PlayerPos_.y += Move_;
+		Move_ += GA_;
 	}
 
-	if (PlayerPos_.y == 450)
+	if (PlayerPos_.y == 350)
 	{
 		if (KeySpace.down())
 		{
-			move_.y = -21.0;
-			PlayerPos_ += move_;
+			Move_ = -17;
+			PlayerPos_.y += Move_;
 		}
 	}
 
-	if (PlayerPos_.y >= 450)
+	if (PlayerPos_.y > 350)
 	{
-		move_.y = 0;
-		PlayerPos_.y = 450;
+		Move_ = 0;
+		PlayerPos_.y = 350;
 	}
 
-	//時間の処理
-	sec_ = 30 - stopwatch_.s();
-	if (sec_ == 0)
+	//タイムアップ処理
+	Sec_ = 30 - Stopwatch_.s();
+	if (Sec_ == 0)
 	{
 		changeScene(State::Score);
 	}
 
+	//移動処理
+	for (int i = 0; i < 3; i++)
+	{
+		if (TreePos_[i].x < -80)
+		{
+			TreePos_[i].x = 900;
+		}
+		else {
+			TreePos_[i].x -= Velocity_;
+		}
+	}
 
+	if (Sec_ % 5 == 0)
+	{
+		Velocity_ += 0.001;
+	}
 }
 
 void Play::draw() const
 {
-	FontAsset(U"GameScore")(sec_).draw(10, 10);
+	BackGround_.draw(0, 0);
+
+	FontAsset(U"GameScore")(Sec_).draw(10, 10);
+
+	for (int i = 0; i < 3; i++)
+	{
+		Tree_.scaled(0.3).drawAt(TreePos_[i]);
+	}
 	PlayerChar_.scaled(0.3).draw(PlayerPos_);
+
 	ItemMoney_.scaled(0.3).drawAt(MoneyPos_);
 }
--- PlayScene.h
+++ PlayScene.h
@@ -14,15 +14,20 @@ public:
 
 private:
 	//level1
-	Stopwatch stopwatch_{ StartImmediately::Yes };			//経過時間
-	int sec_;						//残り時間
-	Vec2 move_;						//走行距離
+	Stopwatch Stopwatch_{ StartImmediately::Yes };			//経過時間
+	int Sec_;						//残り時間
+	double Velocity_;					//移動量
+	int MoveDist_;
+	const Texture BackGround_;		//背景
+	const Texture Tree_;			//背景の木
+	Vec2 TreePos_[3];
 	const Texture PlayerChar_;		//プレイヤーキャラ
 	Vec2 PlayerPos_;				//プレイヤーキャラの座標
-	Vec2 camera_;					//表示位置
+	Vec2 Camera_;					//表示位置
 
 	//level2
 	const double GA_;				//重力加速度
+	double Move_;						//ジャンプの移動量
 	const Texture Life_;			//ライフ
 	Vec2 LifePos_;					//ライフの座標
 
